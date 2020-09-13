b6e26b615b19264b4cd36083d20623ace34980a0
Nguyen Ngan <nguyen.ngan@tt-tech.de>
2020-09-13 15:17:42 +0700

    add theme

---
+++ wp-content/themes/theretailer/theretailer-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theretailer_enqueue_styles', 99 );
+function theretailer_enqueue_styles() {
+
+    wp_enqueue_style( 'the_retailer_styles', get_template_directory_uri() .'/css/styles.css' );
+    wp_enqueue_style( 'stylesheet', get_template_directory_uri() . '/style.css' );
+
+    wp_enqueue_style( 'the-retailer-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'stylesheet' ),
+        wp_get_theme()->get('Version')
+    );
+
+    // enqueue RTL styles
+    if( is_rtl() ) {
+    	wp_enqueue_style( 'the-retailer-child-rtl-styles',  get_template_directory_uri() . '/rtl.css', array( 'the_retailer_styles' ), wp_get_theme()->get('Version') );
+    }
+}
